68c98b86255372451de435c7510438f532377408
T800 <t800gx@gmail.com>
2017-02-25 20:45:53 +0100

    Add files via upload

--- CBXManager/resource.h
+++ CBXManager/resource.h
@@ -1,1 +1,17 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
+#define IDR_MAINFRAME                           128
+#define IDD_MAINDLG                             129
+#define IDD_ABOUTBOX                            130
+#define IDC_SORT_DESC                           1000
+#define IDC_APPABOUT                            1001
+#define IDC_APPLY                               1002
+#define IDC_SORT_ADVOPTGROUP                    1003
+#define IDC_CB_CBZ                              1004
+#define IDC_TH_GROUP                            1005
+#define IDC_CB_RAR                              57681
+#define IDC_CB_ZIP                              57682
+#define IDC_CB_SORT                             57683
+#define IDC_CB_CBR                              57684
