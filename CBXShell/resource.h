d27edd594868811b70f45febc94eb94a6a9ee19a
T800 <t800gx@gmail.com>
2017-02-25 20:44:11 +0100

    Add files via upload

--- CBXShell/resource.h
+++ CBXShell/resource.h
@@ -1,1 +1,7 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
+#define IDR_CBXShell                            102
+#define IDS_PROJNAME                            40000
+#define IDS_CBXSHELL_DESC                       40001
